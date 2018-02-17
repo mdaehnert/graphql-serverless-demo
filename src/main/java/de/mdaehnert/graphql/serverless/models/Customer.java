c187281524050dd16ba05e99756f47dd12f7261f
Michael Dähnert <michael.daehnert@gmail.com>
2018-02-17 14:37:21 +0100

    First response achieved!

---
+++ src/main/java/de/mdaehnert/graphql/serverless/models/Customer.java
@@ -0,0 +1,31 @@
+package de.mdaehnert.graphql.serverless.models;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+
+@DynamoDBTable(tableName = "customer")
+public class Customer {
+
+  private Integer id;
+  private String name;
+  private Integer age;
+  private String birthdate;
+
+  @DynamoDBHashKey(attributeName = "id")
+  public Integer getId() { return id; }
+  public void setId(Integer id) { this.id = id; }
+
+  @DynamoDBAttribute(attributeName="name")
+  public String getName() { return name; }
+  public void setName(String name) { this.name = name; }
+
+  @DynamoDBAttribute(attributeName="age")
+  public Integer getAge() { return age; }
+  public void setAge(Integer age) { this.age = age; }
+
+  @DynamoDBAttribute(attributeName="birthdate")
+  public String getBirthdate() { return birthdate; }
+  public void setBirthdate(String birthdate) { this.birthdate = birthdate; }
+
+}
--- src/main/java/de/mdaehnert/graphql/serverless/resolvers/QueryResolver.java
+++ src/main/java/de/mdaehnert/graphql/serverless/resolvers/QueryResolver.java
@@ -2,31 +2,24 @@ package de.mdaehnert.graphql.serverless.resolvers;
 
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import com.amazonaws.services.dynamodbv2.document.DynamoDB;
-import com.amazonaws.services.dynamodbv2.document.Item;
-import com.amazonaws.services.dynamodbv2.document.spec.QuerySpec;
-import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import de.mdaehnert.graphql.serverless.models.Customer;
 
 public class QueryResolver implements GraphQLQueryResolver {
 
-  private DynamoDB getDB() {
-    AmazonDynamoDBClientBuilder builder = AmazonDynamoDBClientBuilder.standard();
-    builder.withRegion(Regions.EU_CENTRAL_1);
 
-    return new DynamoDB(builder.build());
+  public Customer getCustomer() {
+    return getDB().load(Customer.class, 0);
   }
 
 
-  public String getCustomer() {
-    QuerySpec spec = new QuerySpec()
-      .withKeyConditionExpression("id = :id")
-      .withValueMap(new ValueMap()
-        .withInt(":id", 0));
+  private DynamoDBMapper getDB() {
+    AmazonDynamoDBClientBuilder builder = AmazonDynamoDBClientBuilder.standard();
+    builder.withRegion(Regions.EU_CENTRAL_1);
 
-    Item res = getDB().getTable("customer").query(spec).iterator().next();
-
-    return res.toString();
+    return new DynamoDBMapper(builder.build());
   }
 
 }
