15b5979f2342484368b5e7d2e474ff8373a7bf49
Michael Dähnert <michael.daehnert@gmail.com>
2018-02-17 11:49:51 +0100

    No local installation. Use direct db instead. (sufficient)

--- src/test/java/de/mdaehnert/graphql/serverless/HandlerTest.java
+++ src/test/java/de/mdaehnert/graphql/serverless/HandlerTest.java
@@ -1,42 +1,12 @@
 package de.mdaehnert.graphql.serverless;
 
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.model.*;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.junit.Test;
 
 public class HandlerTest {
 
   @Test
   public void testGetCustomer() {
+    new Handler().handleRequest("", null);
   }
 
-
-  private init() {
-    AmazonDynamoDB ddb = DynamoDBEmbedded.create().amazonDynamoDB();
-  }
-
-
-  private static CreateTableResult createTable(AmazonDynamoDB ddb) {
-    String tableName = "customer";
-    String hashKeyName = "id";
-
-    List<AttributeDefinition> attributeDefinitions = new ArrayList<>();
-    attributeDefinitions.add(new AttributeDefinition(hashKeyName, ScalarAttributeType.S));
-
-    List<KeySchemaElement> ks = new ArrayList<>();
-    ks.add(new KeySchemaElement(hashKeyName, KeyType.HASH));
-
-    ProvisionedThroughput provisionedthroughput = new ProvisionedThroughput(1000L, 1000L);
-
-    CreateTableRequest request =
-      new CreateTableRequest()
-        .withTableName(tableName)
-        .withAttributeDefinitions(attributeDefinitions)
-        .withKeySchema(ks)
-        .withProvisionedThroughput(provisionedthroughput);
-
-    return ddb.createTable(request);
-  }
 }
