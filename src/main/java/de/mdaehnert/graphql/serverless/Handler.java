7ec98ffd2c065f92d4e8f19f89c6934e22a5e777
Michael Dähnert <michael.daehnert@gmail.com>
2018-02-17 11:04:00 +0100

    Starting to test dynamoDB.

--- src/main/java/de/mdaehnert/graphql/serverless/Handler.java
+++ src/main/java/de/mdaehnert/graphql/serverless/Handler.java
@@ -1,12 +1,36 @@
-package de.mdaehner.serverless.graphql;
+package de.mdaehnert.graphql.serverless;
 
-import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.Context; 
 
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.spec.QuerySpec;
+import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
+import com.amazonaws.services.lambda.runtime.Context;
 
 public class Handler {
 
-	public String handleRequest(String input, Context context) {
+  private DynamoDB init() {
+    AmazonDynamoDBClientBuilder builder = AmazonDynamoDBClientBuilder.standard();
+    builder.setRegion(Regions.EU_CENTRAL_1.getName());
 
-	}
+    return new DynamoDB(builder.build());
+  }
+
+
+  public String handleRequest(String input, Context context) {
+    DynamoDB db = init();
+
+
+    QuerySpec spec = new QuerySpec()
+      .withKeyConditionExpression("Id = :id")
+      .withValueMap(new ValueMap()
+        .withString(":id", "1"));
+
+    Item res = db.getTable("Customer").query(spec).iterator().next();
+
+    return res.toString();
+  }
+
 }
--- src/test/java/de/mdaehnert/graphql/serverless/HandlerTest.java
+++ src/test/java/de/mdaehnert/graphql/serverless/HandlerTest.java
@@ -1,27 +1,42 @@
 package de.mdaehnert.graphql.serverless;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.model.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class HandlerTest {
 
   @Test
   public void testGetCustomer() {
-    testGraphQl("""
-      {"query": "
-        query testFind {
-          matches {
-            id
-            notificationSeen
-
-            pdProjectRequest {
-              description
-              projectLocation
-
-              pdProject {
-                customerName
-              }
-            }
-          }
-        }
-      "}
-    """,
-    "{}")
   }
+
+
+  private init() {
+    AmazonDynamoDB ddb = DynamoDBEmbedded.create().amazonDynamoDB();
+  }
+
+
+  private static CreateTableResult createTable(AmazonDynamoDB ddb) {
+    String tableName = "customer";
+    String hashKeyName = "id";
+
+    List<AttributeDefinition> attributeDefinitions = new ArrayList<>();
+    attributeDefinitions.add(new AttributeDefinition(hashKeyName, ScalarAttributeType.S));
+
+    List<KeySchemaElement> ks = new ArrayList<>();
+    ks.add(new KeySchemaElement(hashKeyName, KeyType.HASH));
+
+    ProvisionedThroughput provisionedthroughput = new ProvisionedThroughput(1000L, 1000L);
+
+    CreateTableRequest request =
+      new CreateTableRequest()
+        .withTableName(tableName)
+        .withAttributeDefinitions(attributeDefinitions)
+        .withKeySchema(ks)
+        .withProvisionedThroughput(provisionedthroughput);
+
+    return ddb.createTable(request);
+  }
+}
